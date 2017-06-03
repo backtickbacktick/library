3c5599dbb8d0df1c8eab0dbc1a08e78618ce7027
Brian Reed <me@iambrian.com>
2017-06-02 20:04:43 -0400

    3rd times the charm

--- _library/json-build.js
+++ _library/json-build.js
@@ -5,9 +5,9 @@ const fs = require('fs-extra');
 const path = require('path');
 
 let config = {
-  localPath: './.tmp',
-  cdnUrl: 'https://backtickbacktick.github.io/Library',
-  linkFileExt: ['.js'],
+    localPath: './.tmp',
+    cdnUrl: 'https://backtickbacktick.github.io/library',
+    linkFileExt: ['.js']
 };
 
 console.log('JSON building...');
@@ -18,24 +18,23 @@ fs.ensureDirSync('./.tmp/commands');
 fs.emptyDirSync('./.tmp/commands');
 
 commandDirs.forEach(dir => {
-  fs.copySync('./' + dir, './.tmp/commands/' + dir);
+    fs.copySync('./' + dir, './.tmp/commands/' + dir);
 });
 
 cdcm(config).getData().then(data => {
 
-  let output = {};
+    let output = {};
 
-  data.forEach(kind => {
-    output[kind.type] = kind.items;
-  });
+    data.forEach(kind => {
+        output[kind.type] = kind.items;
+    });
 
-  fs.writeFileSync('./commands.json',
-      JSON.stringify(output.commands, null, '\t').
-          replace(/Library\/master\/commands/g, 'Library/master'));
+    fs.writeFileSync('./commands.json',
+        JSON.stringify(output.commands, null, '\t').replace(/commands\//g, ''));
 
-  fs.removeSync('./.tmp');
-  fs.removeSync('./.tmp-cdcm');
+    fs.removeSync('./.tmp');
+    fs.removeSync('./.tmp-cdcm');
 
-  console.log('JSON built.');
+    console.log('JSON built.');
 
 });
